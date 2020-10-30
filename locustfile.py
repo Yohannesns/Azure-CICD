2cc6239bd8f3323e9cb17f30c7685289e4354bba
Yohannesns <37091801+Yohannesns@users.noreply.github.com>
2020-10-30 05:20:55 -0400

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -6,14 +6,17 @@ class QuickstartUser(HttpUser):
 
     @task
     def index_page(self):
-        self.client.get("/hello")
-        self.client.get("/world")
+        self.client.get("/")
+        self.client.get("/predict")
 
     @task(3)
     def view_item(self):
         for item_id in range(10):
             self.client.get(f"/item?id={item_id}", name="/item")
             time.sleep(1)
-
+     @task()
+    def predict_endpoint(self):
+        self.client.post('/predict', json = test_data)
+        
     def on_start(self):
         self.client.post("/login", json={"username":"foo", "password":"bar"})
